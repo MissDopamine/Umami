1ada6b127fa23d536c338f8987c85cfd27b8ca12
giadasaccucci <giadasac1998@virgilio.it>
2023-03-02 12:53:55 +0100

    graphical part of the grid and button (incomplete)

--- Umami3/decision screen.swift
+++ Umami3/decision screen.swift
@@ -7,104 +7,134 @@
 
 import SwiftUI
 
+
 struct decision_screen: View {
+    
     var body: some View {
-        VStack{
-            VStack{
-                Text("How would you cook?")
-                    .font(.largeTitle)
+        
+        ZStack {
+            
+                    Image("WoodenTable")
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
+                 
+            VStack {
+                Text ("TestText")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
+                    .padding(.bottom, 229.0)
                 
-                
-                HStack{
-                    Button {
-                    } label: {
-                        Image("RiceButton")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
+                Grid{
+                    GridRow{
+                        Button {
+                        } label: {
+                            Image("ChineseRice")
+                                .resizable()
+                                .frame(width: 150.0, height: 150.0)
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        Button {
+                        } label: {
+                            Image("SteamedRice")
+                                .resizable()
+                                .frame(width: 150.0, height: 150.0)
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        Button {
+                        } label: {
+                            Image("PilafRice")
+                                .resizable()
+                                .frame(width: 150.0, height: 150.0)
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        
+                    }
+                    GridRow{
+                        Button {
+                        } label: {
+                            Image("BoiledEgg")
+                                .resizable()
+                                .frame(width: 150.0, height: 150.0)
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        Button {
+                        } label: {
+                            Image("ScrambledEgg")
+                                .resizable()
+                                .frame(width: 150.0, height: 150.0)
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        Button {
+                        } label: {
+                            Image("FriedEgg")
+                                .resizable()
+                                .frame(width: 150.0, height: 150.0)
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        
                     }
                     
-                    Button {
-                    } label: {
-                        Image("RiceButton")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
-                    }
-                    
-                    Button {
-                    } label: {
-                        Image("RiceButton")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
+                    GridRow{
+                        Button {
+                        } label: {
+                            Image("BoiledCarrot")
+                                .resizable()
+                                .frame(width: 150.0, height: 150.0)
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        Button {
+                        } label: {
+                            Image("RawCarrot")
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        Button {
+                        } label: {
+                            Image("")
+                                .resizable()
+                                .frame(width: 0, height: 0)
+                                .foregroundColor(.black)
+                                .position(x:350, y:260)
+                                .shadow(color: .black, radius: 4, x: 2, y: 2)
+                        }
+                        
                     }
                 }
+                .padding([.top, .leading], -330.0)
+                .frame(width: 700.0, height: 400.0)
                 
-                
-                
-                
-                HStack{
-                    Button {
-                    } label: {
-                        Image("BoiledEgg")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
-                    }
-                    
-                    Button {
-                    } label: {
-                        Image("ScrambledEgg")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
-                    }
-                    
-                    Button {
-                    } label: {
-                        Image("FriedEgg")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
-                    }
-                }
-                
-                
-                
-                HStack{
-                    Button {
-                    } label: {
-                        Image("RiceButton")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
-                    }
-                    
-                    Button {
-                    } label: {
-                        Image("RiceButton")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
-                    }
-                    
-                    Button {
-                    } label: {
-                        Image("RiceButton")
-                            .foregroundColor(.black)
-                            .position(x:350, y:260)
-                            .shadow(color: .black, radius: 4, x: 2, y: 2)
-                    }
+                Button{} label: {
+                    Image("ReadyButton")
                 }
                 
             }
-            .padding([.bottom, .trailing], 200.0)
+                    
+                }
+                
+               
             
+            
+            }
         }
-    }
-}
+    
 
 struct decision_screen_Previews: PreviewProvider {
     static var previews: some View {
